1b7596f5ec32e7c04cda0f9241f1cfe4b36865b3
Domenic <56897401+Domenic365@users.noreply.github.com>
2023-07-26 17:23:32 +0200

    added main menu

--- src/scenes/HomeScreen.js
+++ src/scenes/HomeScreen.js
@@ -27,10 +27,18 @@ class HomeScreen extends Phaser.Scene {
 
     createMenuDom() {
         let menu = document.createElement("div");
+        menu.classList.add("menuDiv");
         menu.innerHTML = /*html*/ `
-			<div><img src="" alt=""><button></button></div>
-			<div></div>
+			<div class="top">
+                <img src="./assets/UI/Logo.png" alt="">
+                <button onclick="game.scene.scenes[0].scene.start('Boot')">
+                    <img src="./assets/UI/play.png" alt="">
+                </button>
+            </div>
+			<div class="bottom"></div>
 		`;
+
+        this.add.dom(0, 0, menu);
     }
 
     /* END-USER-CODE */
